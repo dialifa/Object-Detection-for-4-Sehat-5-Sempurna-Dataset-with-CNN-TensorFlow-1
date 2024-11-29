0fb284a49d19053340ba9ea187981cb27f43f200
FAISALAKBARr <faisalzogg022@gmail.com>
2024-11-30 01:21:56 +0700

    memberi tombol ke dataset

--- streamlit_andromeda.py
+++ streamlit_andromeda.py
@@ -311,16 +311,20 @@ if selected == "Home":
     #             st.write("Mengarahkan ke Home...")
     #             st.markdown(f"Pengalihan dibatalkan.")
 
-    # Button to access Kaggle dataset
-    if st.button("Access Dataset on Kaggle"):
-        kaggle_url = "https://www.kaggle.com/datasets/andromedagroup05/data-4-sehat-5-sempurna/data" 
-        # Confirmation dialog
-        user_response = st.radio("Apakah ingin mengunjungi Kaggle?", ["Oke", "Tidak"])
-        if user_response == "Oke":
-            st.success("Mengarahkan ke Kaggle...")
-            st.components.v1.html(f"<script>{kaggle_url}</script>", height=0)
-        elif user_response == "Tidak":
-            st.write("Pengalihan dibatalkan.")
+    # # Button to access Kaggle dataset
+    # if st.button("Access Dataset on Kaggle"):
+    #     kaggle_url = "https://www.kaggle.com/datasets/andromedagroup05/data-4-sehat-5-sempurna/data" 
+    #     # Confirmation dialog
+    #     user_response = st.radio("Apakah ingin mengunjungi Kaggle?", ["Oke", "Tidak"])
+    #     if user_response == "Oke":
+    #         st.success("Mengarahkan ke Kaggle...")
+    #         st.components.v1.html(f"<script>{kaggle_url}</script>", height=0)
+    #     elif user_response == "Tidak":
+    #         st.write("Pengalihan dibatalkan.")
+
+    if st.button("Learn More"):
+        js = "window.open('https://www.kaggle.com/datasets/andromedagroup05/data-4-sehat-5-sempurna/data')"  # Replace with your link
+        st.components.v1.html(f"<script>{js}</script>", height=0)
 
 # Upload Image page
 # elif selected == "Upload Image":
