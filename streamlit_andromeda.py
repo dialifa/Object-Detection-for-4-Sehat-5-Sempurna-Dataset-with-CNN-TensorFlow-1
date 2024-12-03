f2602247a68b0dfaf3d08d9ce363056e439c569b
FAISALAKBARr <faisalzogg022@gmail.com>
2024-12-03 18:38:31 +0700

    indexing

--- streamlit_andromeda.py
+++ streamlit_andromeda.py
@@ -156,10 +156,11 @@ def list_available_cameras(max_cameras=10):
 
 # Global variables
 # CLASS_NAMES = ["sayur", "buah", "minuman", "karbohidrat", "protein"]
+CLASS_NAMES = ["karbohidrat", "buah", "protein", "sayur", "minuman"]
 # CLASS_NAMES = ["buah", "karbohidrat", "minuman", "protein", "sayur"] #index di kaggle 
 # CLASS_NAMES = ['sayur', 'protein', 'minuman', 'karbohidrat', 'buah'] #index di google drive
 
-CLASS_NAMES = ["karbohidrat", "protein", "buah", "sayur", "minuman"]
+# CLASS_NAMES = ["karbohidrat", "protein", "buah", "sayur", "minuman"]
 # CONFIDENCE_THRESHOLD = 0.5
 
 def preprocess_frame(frame, target_size=(224, 224)):
